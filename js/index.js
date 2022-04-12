a04075985f3c575e1f3036531a672890e884a558
Lolo <mguigadev@gmail.com>
2022-04-13 01:36:42 +0200

    confirm

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 let btn;
+let count = 0;
 document.addEventListener("DOMContentLoaded", assignEvents);
 function assignEvents() {
     btn = document.querySelector("button");
     btn.addEventListener("click", draw);
 }
 function draw() {
-    btn.insertAdjacentText("afterend", "asdasddsa");
+    let clicks = count++===1? "vez.":"veces.";
+    document.querySelector("div").innerText = `Hola, Alberto... estoy probando lo que va a ser mi portfolio amateur de mierda... Tío, has pulsado ya ${count} ${clicks}`;
 }
